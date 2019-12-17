949f242f767db91fddf88e586b8acf75791e9ab3
czy <czy71988@163.com>
2019-12-17 17:23:50 +0800

    我的快递金模块

---
+++ components/Used/index.js
@@ -0,0 +1,87 @@
+// components/Used/index.js
+Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+
+	},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+    lists: [
+      {
+        id: 1,
+        amount: '10.00',
+        title:"中通快递抵用金",
+        date: '2019/09/09-202',
+        img: '../../image/used.png',
+      },
+      {
+        id: 1,
+        amount: '10.00',
+        title: "中通快递抵用金",
+        date: '2019/09/09-202',
+        img: '../../image/used.png',
+      },
+      {
+        id: 1,
+        amount: '10.00',
+        title: "中通快递抵用金",
+        date: '2019/09/09-202',
+        img: '../../image/used.png',
+      },
+      {
+        id: 1,
+        amount: '10.00',
+        title: "中通快递抵用金",
+        date: '2019/09/09-202',
+        img: '../../image/used.png',
+      },
+      {
+        id: 1,
+        amount: '10.00',
+        title: "中通快递抵用金",
+        date: '2019/09/09-202',
+        img: '../../image/used.png',
+      },
+      {
+        id: 1,
+        amount: '10.00',
+        title: "中通快递抵用金",
+        date: '2019/09/09-202',
+        img: '../../image/used.png',
+      },
+      {
+        id: 1,
+        amount: '10.00',
+        title: "中通快递抵用金",
+        date: '2019/09/09-202',
+        img: '../../image/used.png',
+      },
+      {
+        id: 1,
+        amount: '10.00',
+        title: "中通快递抵用金",
+        date: '2019/09/09-202',
+        img: '../../image/used.png',
+      },
+      {
+        id: 1,
+        amount: '10.00',
+        title: "中通快递抵用金",
+        date: '2019/09/09-202',
+        img: '../../image/used.png',
+      }
+    ]
+	},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+
+	}
+})
---
+++ components/will-used/index.js
@@ -0,0 +1,100 @@
+// components/will-used/index.js
+Component({
+	/**
+	 * 组件的属性列表
+	 */
+	properties: {
+
+	},
+
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+    lists: [{
+      id: 1,
+      img: '../../image/wil.png',
+      amount: '2.20',
+      title: '中通快递抵用金',
+      date: '2020/01/01'
+    },
+    {
+      id: 2,
+      img: '../../image/wil.png',
+      amount: '9.90',
+      title: '中通快递抵用金',
+      date: '2020/01/01'
+      },
+      {
+        id: 2,
+        img: '../../image/wil.png',
+        amount: '9.90',
+        title: '中通快递抵用金',
+        date: '2020/01/01'
+      },
+      {
+        id: 2,
+        img: '../../image/wil.png',
+        amount: '9.90',
+        title: '中通快递抵用金',
+        date: '2020/01/01'
+      },
+      {
+        id: 2,
+        img: '../../image/wil.png',
+        amount: '9.90',
+        title: '中通快递抵用金',
+        date: '2020/01/01'
+      },
+      {
+        id: 2,
+        img: '../../image/wil.png',
+        amount: '9.90',
+        title: '中通快递抵用金',
+        date: '2020/01/01'
+      },
+      {
+        id: 2,
+        img: '../../image/wil.png',
+        amount: '9.90',
+        title: '中通快递抵用金',
+        date: '2020/01/01'
+      },
+      {
+        id: 2,
+        img: '../../image/wil.png',
+        amount: '9.90',
+        title: '中通快递抵用金',
+        date: '2020/01/01'
+      },
+      {
+        id: 2,
+        img: '../../image/wil.png',
+        amount: '9.90',
+        title: '中通快递抵用金',
+        date: '2020/01/01'
+      },
+      {
+        id: 2,
+        img: '../../image/wil.png',
+        amount: '9.90',
+        title: '中通快递抵用金',
+        date: '2020/01/01'
+      },
+      {
+        id: 2,
+        img: '../../image/wil.png',
+        amount: '9.90',
+        title: '中通快递抵用金',
+        date: '2020/01/01'
+      }
+    ]
+	},
+
+	/**
+	 * 组件的方法列表
+	 */
+	methods: {
+
+	}
+})

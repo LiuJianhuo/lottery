f7241f3b23a87638f8fe22686edc5a3d882c8a15
czy <czy71988@163.com>
2019-12-19 13:45:39 +0800

    my prize update

---
+++ pages/my-prize/modal/index.js
@@ -0,0 +1,28 @@
+// pages/my-prize/modal/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    show: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    closeTap: function (e) {
+      this.triggerEvent('close', e)
+    }
+  }
+})
